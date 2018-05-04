74056980aae205a767df2f2d4b4d2dd260ead119
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-05-03 17:21:25 -0700

    Cambios menores

---
+++ HPATCC.Persistencia/src/mx/gentlepillar/hpatcc/persistencia/entidades/AdministradorPersistencia.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.gentlepillar.hpatcc.persistencia.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+import mx.gentlepillar.hpatcc.nucleo.entidades.Administrador;
+
+/**
+ *
+ * @author jesus
+ */
+public class AdministradorPersistencia {
+    public List<Administrador> getAll(){
+        List<Administrador> administradores = new ArrayList<Administrador>();
+        
+        return administradores;
+    }
+}
