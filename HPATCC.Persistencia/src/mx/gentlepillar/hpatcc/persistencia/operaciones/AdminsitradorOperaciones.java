b6bf2f78669dcde84922a2fecdad6592e26375ed
Jesus Castro Alvarez <Chezz191@gmail.com>
2018-05-06 19:04:52 -0700

    Adminisrador Operaciones

--- HPATCC.Persistencia/src/mx/gentlepillar/hpatcc/persistencia/operaciones/AdminsitradorOperaciones.java
+++ HPATCC.Persistencia/src/mx/gentlepillar/hpatcc/persistencia/operaciones/AdminsitradorOperaciones.java
@@ -5,14 +5,30 @@
  */
 package mx.gentlepillar.hpatcc.persistencia.operaciones;
 
+import mx.gentlepillar.chuyptionpane.vistas.ChuyptionPane;
+import mx.gentlepillar.hpatcc.nucleo.entidades.Administrador;
+import mx.gentlepillar.hpatcc.persistencia.entidades.AdministradorPersistencia;
+
 /**
  *
  * @author jesus
  */
 public class AdminsitradorOperaciones {
+    AdministradorPersistencia adminsPers = new AdministradorPersistencia();
+    Administrador administrador = null;
+    ChuyptionPane chuyPane = new ChuyptionPane();
     public boolean verify(String noControl, char[] contrasenia){
         boolean validacion = false;
-        
+        administrador = adminsPers.getByNumControl(noControl);
+        if (administrador != null) {
+            if (contrasenia.equals(administrador.getContrasenia())) {
+                validacion = true;
+            }else{
+                chuyPane.showMessage(null, "La contrasenia es incorrecta", "Error");
+            }
+        }else{
+            chuyPane.showMessage(null, "No se ha encontrador el administrador solicitado", "Error");
+        }
         return validacion;
     }
 }
